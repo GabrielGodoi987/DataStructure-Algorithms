77514933c0c882e538c1216d246ce2b26d126021
GanbrielGodoi390 <110495432+GabrielGodoi987@users.noreply.github.com>
2024-09-11 08:33:00 -0500

    Stack sorting using temporary stack

---
+++ Ex01/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "pilha.h"
+
+int main(void)
+{
+    int n, numeros, maior = 0, j = 0;
+    puts("Digite o tamanho da sequencia:");
+    scanf("%d", &n);
+    Pilha a = pilha(n * sizeof(int));
+    Pilha b = pilha(n * sizeof(int));
+
+    for (int k = 0; k < n; k++)
+    {
+        puts("Digite os números da sequência");
+        scanf("%d", &numeros);
+        empilha(numeros, a);
+    }
+
+    while (!vaziap(a))
+    {
+        int k = topo(a);
+        desempilha(a);
+
+        while (!vaziap(b) && topo(b) < k)
+        {
+            empilha(topo(b), a);
+            desempilha(b);
+        }
+
+        empilha(k, b);
+    }
+
+    puts("");
+
+    puts("\nSequência ordenada:");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", a->item[i]);
+    }
+
+    printf("o topo dessa merda é %d\n", topo(b));
+
+    return 0;
+}
