3524bb3095749a3c8b0548fd2c85ca226cacc939
GanbrielGodoi390 <110495432+GabrielGodoi987@users.noreply.github.com>
2024-09-12 12:00:53 -0500

    Reverse word with stack

--- Ex03/main.c
+++ Ex03/main.c
@@ -10,35 +10,28 @@ int main(void)
   fgets(str, LIMITSTRING, stdin);
   char *pch;
   int length = strlen(str), j = 0, k = 0;
-  Pilha A = pilha(length);
-  Pilha B = pilha(length);
+  Pilha A = pilha(LIMITSTRING);
+  Pilha B = pilha(LIMITSTRING);
 
-  printf("Quebrando a string \"%s\" em uma pilha de letras:\n", str);
   pch = strtok(str, " ,.-!");
   while (pch != NULL)
   {
     for (int i = 0; i < strlen(pch); i++)
     {
-      printf("%c\n", pch[i]);
       empilha(pch[i], B);
     }
-    puts("Quero ver o topo de B a cada iteração");
     while (!vaziap(B))
     {
-      printf("%c\n", topo(B));
       empilha(topo(B), A);
       desempilha(B);
+      //printf("%c", topo(A));
     }
-    empilha(" ", B);
+    if (!vaziap(A))
+      empilha(" ", A);
     pch = strtok(NULL, " ,.-");
   }
 
-  puts("");
+  printf("%c", topo(A));
 
-  while (!vaziap(A))
-  {
-    printf("%c", topo(A));
-    desempilha(A);
-  }
   return 0;
 }
