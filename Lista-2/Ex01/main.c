5795acd406f83db8bd2fe1eac605bde178b7b643
GanbrielGodoi390 <110495432+GabrielGodoi987@users.noreply.github.com>
2024-10-01 08:29:46 -0500

    :us: updates

--- Lista-2/Ex01/main.c
+++ Lista-2/Ex01/main.c
@@ -1,112 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include "pilha.h"
-#define lengthMax 513
 
-int prio(char digit)
-{
-    switch (digit)
-    {
-    case '(':
-        return 0;
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    }
-
-    return -1;
-}
-
-char *postfix(char *e)
+char *posfixa(char *e)
 {
     static char s[256];
     int j = 0;
     Pilha P = pilha(256);
     for (int i = 0; e[i]; i++)
-    {
-        if (e[i] == '(')
-            empilha('(', P);
-        else if (isdigit(e[i]))
+        if (isdigit(e[i]))
             s[j++] = e[i];
-        else if (strchr("+-/*", e[i]))
-        {
-            while (!vaziap(P) && prio(topo(P)) >= prio(e[i]))
-                s[j++] = desempilha(P);
+        else if (strchr("+*-/", e[i]))
             empilha(e[i], P);
-        }
         else if (e[i] == ')')
-        {
-            while (topo(P) != '(')
-                s[j++] = desempilha(P);
-            desempilha(P);
-        }
-    }
-    while (!vaziap(P))
-    {
-        s[j++] = desempilha(P);
-    }
+            s[j++] = desempilha(P);
     s[j] = '\0';
     destroip(&P);
     return s;
 }
 
-int eval(char *str)
-{
-    int value = 0;
-    Pilha p = pilha(lengthMax);
-    for (int i = 0; i < strlen(str); i++)
-    {
-        if (isdigit(str[i]))
-        {
-            empilha(str[i] - '0', p);
-        }
-        else if (strchr("+-/*", str[i]))
-        {
-            int n2 = desempilha(p);
-            int n1 = desempilha(p);
-            if (str[i] == '*')
-            {
-                value = n1 * n2;
-                empilha(value, p);
-                value = 0;
-            }
-            else if (str[i] == '/')
-            {
-                value = n1 / n2;
-                empilha(value, p);
-                value = 0;
-            }
-            else if (str[i] == '-')
-            {
-                value = n1 - n2;
-                empilha(value, p);
-                value = 0;
-            }
-            else if (str[i] == '+')
-            {
-                value = n1 + n2;
-                empilha(value, p);
-                value = 0;
-            }
-        }
-    }
-
-    return desempilha(p);
-    destroip(&p);
-}
-
 int main(void)
 {
-    char e[lengthMax] = "(2*3)+(8/4)";
-
-    char *val = postfix(e);
-    printf("Posfixa: %s\n", val);
-
-    printf("posfixa resolvida %d\n", eval(val));
+    char e[513];
+    printf("Infixa? ");
+    fgets(e, 513, stdin);
+    printf("Posfixa: %s\n", posfixa(e));
     return 0;
 }
